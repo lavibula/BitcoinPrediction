fff279795a158e0f92995ad0f41f429bc05941eb
Lavibuu La <121961129+lavibula@users.noreply.github.com>
2023-04-17 23:59:37 +0700

    random forest testing

--- data/CRAWL/main.py
+++ data/CRAWL/main.py
@@ -4,19 +4,7 @@ import pandas as pd
 merged_df = pd.DataFrame()
 
 # Đọc và merge các file Excel
-for i in range(1, 19):
-    filename = f"CRAWL/data/data{i}.xlsx"  # tên file Excel
-    df = pd.read_excel(filename)
-    df = df.rename(columns={"time": "time{i}"})
-    df = df.drop(columns=['t'])
-    
-    if merged_df.empty:
-        merged_df = df
-    else:
-        merged_df = pd.merge(merged_df, df, on='time{i}', how='outer')
+data_excel = pd.read_excel("working/Data_working.xlsx")
 
-# Loại bỏ các cột trùng lặp (nếu có)
-
-# Lưu kết quả vào file Excel
-merged_df.to_excel("merged_data.xlsx", index=False)
-print(merged_df.head())
+# Chuyển đổi sang định dạng CSV và lưu vào tệp mới
+data_excel.to_csv("working/Data_working.csv", index=False)
