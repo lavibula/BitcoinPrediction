49b47d666d0c79ac844f440c86ee11edecfc7341
LongNT2003 <130909210+LongNT2003@users.noreply.github.com>
2023-06-23 20:08:08 +0700

    a

--- Evaluation/evaluation.py
+++ Evaluation/evaluation.py
@@ -6,12 +6,7 @@ prediction_train = 0
 prediction_test = 1
 true_test = 1
 
-def calculate_acc(true_values, predicted_values):
-    count = 0
-    for i in range(1, len(true_values)):
-        if (true_values[i] - true_values[i-1]) * (predicted_values[i] - predicted_values[i-1]) > 0:
-            count += 1
-    return count / (len(true_values) - 1)
+
 
 def print_evaluation_results(true_values, predicted_values, dataset_name):
     print("Test ACCuracy for", dataset_name)
@@ -23,11 +18,6 @@ def print_evaluation_results(true_values, predicted_values, dataset_name):
     # MAPE
     mape = mean_absolute_percentage_error(true_values, predicted_values)
     print("Mean Absolute Percentage Error (MAPE): {:.4f}".format(mape))
-    
-    # ACC
-    acc = calculate_acc(true_values, predicted_values)
-    print("ACC test: {:.4f}".format(acc))
-    print()
 
 # Print evaluation results for the train set
 print_evaluation_results(true_train, prediction_train, "train set")
